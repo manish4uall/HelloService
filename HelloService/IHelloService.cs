6093de33b079faece3f34c42a6e9b46871cea4e5
Manish Verma <mr.manish4uall@gmail.com>
2019-01-06 18:27:08 +0530

    INIT

---
+++ HelloService/IHelloService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHelloService" in both code and config file together.
+
+    [ServiceContract] // By Decorating this interface with ServiceContract we are turning this into WCF Service
+    public interface IHelloService
+    {
+        [OperationContract] // By Decorating it we are making this Method available as part of Service to the Client
+        string GetMessage(string name);
+    }
+}
